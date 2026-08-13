eb681ddd4a43a86a1aa7cb18bd439c1aa75c87b3
Henrvas <henry.felipe@yahoo.com>
2026-08-13 13:31:21 -0300

    Update schema and add new tables for educational institutes, enrolloments, invoices, and students

--- app/models/enrolloment.rb
+++ app/models/enrolloment.rb
@@ -26,4 +26,3 @@ def create_invoices
   end
 end
 
-def 
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,47 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_11_143317) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_13_131307) do
+  create_table "educational_institutes", force: :cascade do |t|
+    t.string "cnpj"
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.string "school_type"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "enrolloments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.decimal "full_price_course"
+    t.decimal "invoice_due_date"
+    t.string "name_course"
+    t.decimal "number_invoices"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invoices", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "invoice_status"
+    t.string "price_invoice"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "produtos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "descricao"
     t.string "nome"
     t.decimal "preco"
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.decimal "cell_number"
+    t.string "cnpj"
+    t.datetime "created_at", null: false
+    t.date "date_of_birth"
+    t.string "gender"
+    t.string "name"
+    t.string "payment_method"
     t.datetime "updated_at", null: false
   end
 end
